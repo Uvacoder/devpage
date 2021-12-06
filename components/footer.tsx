ea1b2341cb1461172fbd606475cfb5bf9ffd00b2
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2021-12-05 22:25:43 -0500

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-center lg:px-8">
         <div className="mt-8 md:mt-0 md:order-1">
           <p className="text-center text-base dark:text-gray-400  ">
-            &copy; {new Date().getFullYear()} Alexander May.{" "}
+            &copy; {new Date().getFullYear()} uvacoder.{" "}
             {t("all_rights_reserved")}
           </p>
         </div>
